64a17775d978ba18c46dd9d8c9427b0b1bb9111e
romulo bordezani <romulobordezani@gmail.com>
2018-06-01 04:18:49 -0300

    GFG-29199 - Fixing Exporter with babel-cli

--- src/exporter.js
+++ src/exporter.js
@@ -1,4 +1,4 @@
-export { Button } from './elements/Button';
+export { default as Button } from './elements/Button';
 export { ThemeContext, Theme, withTheme }from './providers/ThemeProvider';
-export { CssBaseline } from './providers/CssBaseline';
-export { DefaultColors } from './core/Colors';
+export { default as CssBaseline } from './providers/CssBaseline';
+export { default as DefaultColors } from './core/Colors';
