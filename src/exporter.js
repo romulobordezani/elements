ecb75469dd4e3730266cf045644729bdd8c26b69
romulo bordezani <romulobordezani@gmail.com>
2018-06-01 15:56:52 -0300

    GFG-29199 - Adding Typography to exporter

--- src/exporter.js
+++ src/exporter.js
@@ -1,4 +1,5 @@
 export { default as Button } from './elements/Button';
-export { ThemeContext, Theme, withTheme }from './providers/ThemeProvider';
+export { ThemeContext, Theme, withTheme } from './providers/ThemeProvider';
 export { default as CssBaseline } from './providers/CssBaseline';
 export { default as DefaultColors } from './core/Colors';
+export { default as Typograpy } from './providers/Typography';
