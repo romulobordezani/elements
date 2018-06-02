afc09b4d485f419dd43e42f9414d336be9bdbd78
romulo bordezani <romulobordezani@gmail.com>
2018-06-02 03:41:42 -0300

    GFG-29199 - Adding docs folder

--- src/viewer/components/ViewerHeader/index.jsx
+++ src/viewer/components/ViewerHeader/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ThemeChangerContainer from '../ThemeChanger/ThemeChangerContainer';
-import logo from './images/logo-elem.gif';
+
 import logoType from './images/elem_type.svg';
 import { ThemeContext } from '../../../providers/ThemeProvider';
 import './ViewerHeader.css';
@@ -11,7 +11,7 @@ function ViewerHeader() {
       {({ theme }) => (
         <header className="header" style={{ backgroundColor: theme.colors.dark[700] }}>
           <div className="header__logo-type">
-            <img src={logo} className="header__logo-type__logo header__logo-type__logo_size_small" alt="Elem" />
+            <div className="header__logo-type__logo header__logo-type__logo_size_small" alt="Elem" />
             <div className="header__logo-type__title"><img src={logoType} alt="Elements" /></div>
           </div>
           <div className="header__theme-switcher" ><ThemeChangerContainer /></div>
